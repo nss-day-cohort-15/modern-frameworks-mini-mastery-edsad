589f29f7fec2718938f9bf7f4e0c2d6c55512be2
Ed Sadler <ed_lorie@comcast.net>
2016-09-12 16:01:14 -0500

    all I can do

---
+++ app/itemCtrl.js
@@ -0,0 +1,9 @@
+"use strict";
+
+app.controller("itemCtrl", function($scope) {
+    $scope.itemCtrl = [
+        {name: "Name"},
+        {name: "Description"},
+        {name: "Price"}
+    ];
+});
